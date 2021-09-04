4413b6262ce91fe163c5e428628d946e5aa7195f
unknown <wq.lin@winrobot360.com>
2021-09-04 17:54:22 +0800

    9-4 下班

--- GinStudy/Controller/UserController.go
+++ GinStudy/Controller/UserController.go
@@ -5,11 +5,13 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	. "github.linwenqiang.com/GinStudy/Dto" //.表示调用公共方法不需要加前缀
+	dtoModel "github.linwenqiang.com/GinStudy/Dto" //.表示调用公共方法不需要加前缀
 )
 
+var controlelrName = "/User"
+
 //相当于action方法
-func Login(user UserDto) bool {
+func Login(user dtoModel.UserDto) bool {
 	if user.Username == "1" && user.Password == "2" {
 		return true
 	} else {
@@ -20,10 +22,11 @@ func Login(user UserDto) bool {
 //相当于一个controller 绑定该conterller下面的action
 func BindingUserControllerRouting(engine *gin.Engine) {
 
-	engine.POST("/login", func(context *gin.Context) {
+	engine.POST(controlelrName+"/login", func(context *gin.Context) {
+		fmt.Println("参数 username:", context.PostForm("username"))
 		//这一部分相当于前置处理
-		var user UserDto
-		err := context.BindQuery(&user)
+		var user dtoModel.UserDto
+		err := context.BindQuery(user)
 		if err != nil {
 			log.Fatal(err.Error())
 			println("获取参数出错：" + err.Error())
--- GinStudy/main.go
+++ GinStudy/main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
-	. "github.linwenqiang.com/GinStudy/Controller"
+	//. "github.linwenqiang.com/GinStudy/Controller"
 )
 
+type UserDto struct {
+	Username string `form:"username"`
+	Password string `form:"password"`
+}
+
+func Login(user UserDto) bool {
+	if user.Username == "1" && user.Password == "2" {
+		return true
+	} else {
+		return false
+	}
+}
 func main() {
 	engine := gin.Default()
 
-	BindingUserControllerRouting(engine)
+	//BindingUserControllerRouting(engine)
+
+	engine.POST("/User/login", func(context *gin.Context) {
+		fmt.Println("参数 username:", context.PostForm("username"))
+		//这一部分相当于前置处理
+		var user UserDto
+		err := context.BindQuery(user)
+		if err != nil {
+			log.Fatal(err.Error())
+			println("获取参数出错：" + err.Error())
+		}
+		fmt.Println(" Username = ", user.Username)
+		fmt.Println(" Password = ", user.Password)
+		result := Login(user)
+		if result {
+			context.Writer.Write([]byte("hello," + user.Username + " success login"))
+		} else {
+			context.Writer.Write([]byte("hello," + user.Password + " fail"))
+		}
+	})
 
 	err := engine.Run(":8090")
 	if err != nil {
