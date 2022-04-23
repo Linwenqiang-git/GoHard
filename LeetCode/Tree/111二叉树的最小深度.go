3c8879ab68a8d2592f6b10de31c55845b784de8f
linwenqiang <648808699@qq.com>
2022-04-23 22:10:10 +0800

    4-23

--- LeetCode/Tree/111二叉树的最小深度.go
+++ LeetCode/Tree/111二叉树的最小深度.go
@@ -1,22 +1,25 @@
 package tree
 
 import (
+	"math"
+
 	utility "github.linwenqiang.com/LeetCode/Utility"
 )
 
 func minDepth(root *TreeNode) int {
-	return deap(root)
-}
-
-func deap(root *TreeNode) int {
-	deep := 1
-	if root.Left == nil || root.Right == nil {
-		return deep
-	} else {
-		leftHeight := 0
-		rightHeight := 0
-		leftHeight += minDepth(root.Left) + 1
-		rightHeight += minDepth(root.Right) + 1
-		return utility.Min(leftHeight, rightHeight)
+	if root == nil {
+		return 0
 	}
+	//深度优先，找到叶子节点
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	min := math.MaxInt32
+	if root.Left != nil {
+		min = utility.Min(minDepth(root.Left), min)
+	}
+	if root.Right != nil {
+		min = utility.Min(minDepth(root.Right), min)
+	}
+	return min + 1 //+1是加上自己本身
 }
